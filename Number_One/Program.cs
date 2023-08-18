1a70c02f0eeef085dfc556edca275d21bc9701ea
Gaffarov_Daniel <daniel.gaffarov@mail.ru>
2023-08-18 20:20:36 +0300

    Здание №1

---
+++ Number_One/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Ввидите первое число");
+double first_number = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+double second_number = Convert.ToDouble(Console.ReadLine());
+
+if (first_number > second_number)
+{
+    Console.WriteLine($"a = {first_number}; b = {second_number}; Max = {first_number}");
+}
+
+if (first_number < second_number)
+{
+    Console.WriteLine($"a = {first_number}; b = {second_number}; Max = {second_number}");
+}
