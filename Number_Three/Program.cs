629f986887bacbd9d12e38ed662b94496a894aec
Gaffarov_Daniel <daniel.gaffarov@mail.ru>
2023-08-22 12:39:12 +0300

    3 задача

---
+++ Number_Three/Program.cs
@@ -0,0 +1,9 @@
+﻿double number = Convert.ToDouble(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Conosle.WeriteLine($"{number} -> Да");
+}
+else
+{
+    Console.WeriteLine($"{number} -> Нет")
+}
