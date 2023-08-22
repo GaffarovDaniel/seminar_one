96ed10106bb665484fe8d1428192ac06915776c7
Gaffarov_Daniel <daniel.gaffarov@mail.ru>
2023-08-22 12:25:31 +0300

    Вторая задача

--- Number_One/Program.cs
+++ Number_One/Program.cs
@@ -4,11 +4,10 @@ Console.WriteLine("Введите второе число");
 double second_number = Convert.ToDouble(Console.ReadLine());
 
 if (first_number > second_number)
-{
     Console.WriteLine($"a = {first_number}; b = {second_number}; Max = {first_number}");
-}
 
-if (first_number < second_number)
-{
+else if (first_number < second_number)
     Console.WriteLine($"a = {first_number}; b = {second_number}; Max = {second_number}");
-}
+
+else
+    Console.WriteLine($"Числа равны");
