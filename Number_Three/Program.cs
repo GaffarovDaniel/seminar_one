e35bd26212daf43036bb2e94991e715470ad6922
Gaffarov_Daniel <daniel.gaffarov@mail.ru>
2023-08-22 13:54:09 +0300

    Исправление ошибок

--- Number_Three/Program.cs
+++ Number_Three/Program.cs
@@ -1,9 +1,9 @@
 ﻿double number = Convert.ToDouble(Console.ReadLine());
 if (number % 2 == 0)
 {
-    Conosle.WeriteLine($"{number} -> Да");
+    Console.WriteLine($"{number} -> Да");
 }
 else
 {
-    Console.WeriteLine($"{number} -> Нет")
+    Console.WriteLine($"{number} -> Нет");
 }
