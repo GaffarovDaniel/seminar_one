2ae264eb826178785dba47c3fa942658d4e70782
Gaffarov_Daniel <daniel.gaffarov@mail.ru>
2023-08-22 13:23:18 +0300

    Задача номер 5

---
+++ Number_Five/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+int number_two = number; // переменная для рассчета + чтоб красиво вывести
+int result = 0;
+if (number < 100)
+{
+    Console.WriteLine($"{number} -> нет");
+}
+else 
+{
+    while (number_two > 999)
+    {
+        number_two /= 10;
+    }
+    result = number_two % 10;
+    Console.WriteLine($"{number} -> {result}");
+}
