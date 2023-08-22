7e6cd8d191188a0daefc7a7306b12c5965b851ca
Gaffarov_Daniel <daniel.gaffarov@mail.ru>
2023-08-22 12:27:31 +0300

    Добавление новой задачи

---
+++ Number_Two/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Ввидите 1 число");
+double first_number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ввидите 2 число");
+double second_number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ввидите 3 число");
+double third_number = Convert.ToInt32(Console.ReadLine());
+if(first_number > second_number)
+{
+    if(first_number > third_number)
+        Console.WriteLine($"{first_number} Максимальное число");
+    else
+        Console.WriteLine($"{third_number} Максимальное число");
+}
+else if (first_number < second_number)
+{
+    if(second_number > third_number)
+        Console.WriteLine($"{second_number} Максимальное число");
+    else
+        Console.WriteLine($"{third_number} Максимальное число");
+}
+else
+{
+    Console.WriteLine("Числа равны");
+}
